f84bae3129affa0a46120bf9d28edee936429d69
mukeshbcc <mukesh.kumar@brisbane.qld.gov.au>
2019-07-09 17:06:22 +1000

    Windows Console Webhook to detect the Mouse click , Both inside and outside the application.

--- Global Mouse Hooks/ClickDetector.cs
+++ Global Mouse Hooks/ClickDetector.cs
@@ -1,9 +1,11 @@
-﻿using Gma.System.MouseKeyHook;
+﻿using Global_Mouse_Hooks;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Gma.System.MouseKeyHook;
+using System.Windows.Forms;
 
 namespace Global_Mouse_Hooks
 {
@@ -17,11 +19,13 @@ namespace Global_Mouse_Hooks
             Hook.GlobalEvents().MouseDown += async (sender, e) =>
             {
                 Console.WriteLine($"Mouse {e.Button} Down");
+                Console.WriteLine("Co-ordinates of Mouse: "+e.X + " "+e.Y);
             };
             //When a double click is made
             Hook.GlobalEvents().MouseDoubleClick += async (sender, e) =>
             {
                 Console.WriteLine($"Mouse {e.Button} button double clicked.");
+                Console.WriteLine("Co-ordinates of Mouse: " + e.X + " " + e.Y);
             };
         }
     }
